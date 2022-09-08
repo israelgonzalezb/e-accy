7e04d1ce4de2b7abc711a8ade3cb6bdc09b57696
Israel Gonzalez-Brooks <48233351+israelgonzalezb@users.noreply.github.com>
2022-09-07 18:06:21 -0700

    fix: sanitize req path

--- src/main.rs
+++ src/main.rs
@@ -3,40 +3,22 @@ use actix_web::{server, App, HttpRequest, Responder, fs};
 // Import the std::env crate.
 use std::env;
 
-// Create a function that will return a requested file from the /static dir
-fn respond(req: &HttpRequest) -> impl Responder {
-    
-    // Get the /{to} str
-    let to = req.match_info().get("name").unwrap()
-    let path = if to.is_empty() { "/" } else { path };
-    let fileRequested = format!("static/{}.html", path)
-    
-    // Open the corresponding html file if it exists, otherwise serve null.html
-    fs::NamedFile::open(fileRequested).unwrap())
+fn respond(req: &HttpRequest) -> Result<fs::NamedFile, Error> {
+    let path = req.match_info().get("name").unwrap_or("index").to_string();
+    let path = Path::new(&path);
+    let path = path.to_str().unwrap();
+    let file_requested = format!("static/{}.html", path); // Fix this
+    fs::NamedFile::open(file_requested) // This is risky too
 }
 
 fn main() {
-    // Get the port number from the environment variable.
-    let port = env::var("PORT")
-        // If the environment variable is not set, use port 3000.
-        .unwrap_or_else(|_| "3000".to_string()) // Has something to do with monads lol
-        // Parse the port number as a number.
-        .parse()
-        // If the port number is not a number, panic.
-        .expect("PORT must be a number");
+    let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string()).parse().expect("PORT must be a number");
 
-    // Create a new server.
     server::new(|| {
-        // Create a new application.
         App::new()
-            // Create a new resource and route
-            // Use the respond function to return a file
             .resource("/", |r| r.f(respond))
     })
-     // Bind the server to the port.
     .bind(("0.0.0.0", port))
-     // If the server can not bind to the port, panic.
     .expect("Can not bind to port 8000")
-    // Run that sth homie.
     .run();
 }
