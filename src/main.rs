c8346ee1a85959ddf23e7e285ceb9334b8cefb08
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2023-03-17 14:01:21 -0700

    chore: refactor to upgrade to actix 4

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,10 @@
-// Import the actix_web crate.
-use actix_web::{server, App, HttpRequest, Responder, fs, Path };
-// Import the std::env crate.
-use std::env;
+use actix_files as fs;
+use actix_web::{App, HttpServer};
 
-fn respond(req: &HttpRequest) -> impl Responder {
-    let path = req.path(); // .unwrap_or("index")
-    // let path = Path::new(&path);
-    // let path = path.to_str().unwrap();
-    let file_requested = format!("static/{}.html", path); // Fix this
-    fs::NamedFile::open(file_requested) // This is risky too
-}
-
-fn main() {
-    let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string()).parse().expect("PORT must be a number");
-
-    server::new(|| {
-        App::new()
-            .resource("*", |r| r.f(respond))
-    })
-    .bind(("0.0.0.0", port))
-    .expect("Can not bind to port 8000")
-    .run();
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(fs::Files::new("/", "./static").index_file("index.html").show_files_listing()))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
