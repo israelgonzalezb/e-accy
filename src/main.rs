5cc3bade1f89919da74e625cd1f511ac29c7f946
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2023-03-17 14:46:03 -0700

    fix: bind to env port

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use actix_files as fs;
 use actix_web::{App, HttpServer};
+use std::env
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string()).parse().expect("PORT must be a number");
+
     HttpServer::new(|| App::new().service(fs::Files::new("/", "./static").index_file("index.html").show_files_listing()))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("127.0.0.1", port))?
         .run()
         .await
 }
